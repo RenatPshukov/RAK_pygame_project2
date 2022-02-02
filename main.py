84d5770eced7b6f88b3bccffcf42d9e53b034ab7
Renat <renat_pshukov06@mail.ru>
2022-02-02 21:43:16 +0300

    Обновление!! В код добавлены спрайты и работа с ними(изменение размеров и т.д).  Решена проблема с нулевой скоростью шаров! Теперь она разная для всех шаров как и цвета. В коде расписаны комментарии и ТЗ доведена до финальной версий (ТЗ готов).

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # TODO Импортируем нужные библиотеки
 import os
 import pygame
-from random import randint, randrange
+from random import randint, choice
 
 pygame.display.set_caption('Insanity balls')  # название
-size = width, height = 1400, 900  # размеры окна
+# устанавливаем размер окна
+size = width, height = 1400, 900
 screen = pygame.display.set_mode(size)
+# Используется для управления fps
 clock = pygame.time.Clock()
 
 
-# TODO загрузка спрайта
+# TODO функция загрузки спрайтов
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
     try:
@@ -29,16 +31,33 @@ def load_image(name, color_key=None):
 
 # TODO класс шарика
 class Ball(pygame.sprite.Sprite):
+
     def __init__(self, group, radius, x, y):
         # НЕОБХОДИМО вызвать конструктор родительского класса Sprite
+        red_ball = pygame.transform.scale(load_image("red_ball.png"), (radius, radius))
+        blue_ball = pygame.transform.scale(load_image("blue_ball.png"), (radius, radius))
+        darkblue_ball = pygame.transform.scale(load_image("darkblue_ball.png"), (radius, radius))
+        green_ball = pygame.transform.scale(load_image("green_ball.png"), (radius, radius))
+        orange_ball = pygame.transform.scale(load_image("orange_ball.png"), (radius, radius))
+        pink_ball = pygame.transform.scale(load_image("pink_ball.png"), (radius, radius))
+        purple_ball = pygame.transform.scale(load_image("purple_ball.png"), (radius, radius))
+        white_ball = pygame.transform.scale(load_image("white_ball.png"), (radius, radius))
+        yellow_ball = pygame.transform.scale(load_image("yellow_ball.png"), (radius, radius))
+        s = [red_ball, blue_ball, darkblue_ball, green_ball, orange_ball, pink_ball, purple_ball, white_ball, yellow_ball]
         super().__init__(group)
-        self.image = pygame.Surface((2 * radius, 2 * radius),
-                                    pygame.SRCALPHA, 32)
-        pygame.draw.circle(self.image, pygame.Color((randint(0, 255), randint(0, 255), randint(0, 255))),
-                           (radius, radius), radius)
+        ds = choice(s)
+        self.image = ds
+        # self.image = pygame.Surface((2 * radius, 2 * radius),
+        # pygame.SRCALPHA, 32)
+        # pygame.draw.circle(self.image, pygame.Color((randint(0, 255), randint(0, 255), randint(0, 255))),
+        # (radius, radius), radius)
         self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
-        self.vx = randint(-3, 6)
-        self.vy = randrange(-3, 6)
+        self.vx = randint(-13, 13)
+        self.vy = randint(-13, 13)
+        while self.vx == 0:
+            self.vx = randint(-13, 13)
+        while self.vy == 0:
+            self.vy = randint(-13, 13)
 
     def update(self):
         self.rect = self.rect.move(self.vx, self.vy)
@@ -72,6 +91,7 @@ class Border(pygame.sprite.Sprite):
 # группа, содержащая все спрайты
 all_sprites = pygame.sprite.Group()
 
+
 # создаём границы
 Border(5, 5, width - 5, 5)
 Border(5, height - 5, width - 5, height - 5)
@@ -80,7 +100,8 @@ Border(width - 5, 5, width - 5, height - 5)
 
 # создаём шары
 for i in range(50):
-    Ball(all_sprites, randint(15, 30), randint(20, 1380), randint(20, 880))
+    Ball(all_sprites, randint(30, 70), randint(0, 1400), randint(0, 900))
+print(all_sprites)
 
 running = True
 # TODO основной цикл
@@ -89,12 +110,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     # задаём задний фон
-    screen.fill(pygame.Color("white"))
+    # screen.fill(pygame.Color('white'))
+    bg = pygame.image.load('data\\bg3.png')
+    screen.blit(bg, (0, 0))
+    # Обновляем спрайты
     all_sprites.update()
+    # Рисуем объекты на окне
     all_sprites.draw(screen)
+    # Обновляем экран после рисования объектов
     pygame.display.flip()
     # FPS
-    clock.tick(100)
+    clock.tick(30)
 
 # выход
 pygame.quit()
