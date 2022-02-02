ddcd2475f2b0bf9d882e6d814148802fd2a04763
Program-F1le <kaurbekkambolov@yandex.ru>
2022-02-02 19:17:36 +0300

    совмещение курсора и шариков

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
+import random
 import pygame
+
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+
+clock = pygame.time.Clock()
 
 
 def load_image(name, color_key=None):
-    fullname = os.path.join('data', name)
+    fullname = os.path.join(name)
     try:
         image = pygame.image.load(fullname).convert()
     except pygame.error as message:
@@ -19,39 +25,76 @@ def load_image(name, color_key=None):
     return image
 
 
-def main():
-    size = 400, 300
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Свой курсор мыши')
+class Ball(pygame.sprite.Sprite):
 
-    # группа, содержащая все спрайты
-    all_sprites = pygame.sprite.Group()
+    def __init__(self, group, radius, x, y):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite
+        super().__init__(group)
+        self.image = pygame.Surface((2 * radius, 2 * radius),
+                                    pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("red"),
+                           (radius, radius), radius)
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = random.randint(-5, 5)
+        self.vy = random.randrange(-5, 5)
 
-    # изображение должно лежать в папке data
-    cursor_image = load_image("cursor.png")
-    cursor = pygame.sprite.Sprite(all_sprites)
-    cursor.image = cursor_image
-    cursor.rect = cursor.image.get_rect()
-
-    # скрываем системный курсор
-    pygame.mouse.set_visible(False)
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEMOTION:
-                # изменяем положение спрайта-стрелки
-                cursor.rect.topleft = event.pos
-        screen.fill(pygame.Color("black"))
-        # рисуем курсор только если он в пределах окна
-        if pygame.mouse.get_focused():
-            all_sprites.draw(screen)
-        pygame.display.flip()
-
-    pygame.quit()
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
+        if pygame.sprite.spritecollideany(self, horizontal_borders):
+            self.vy = -self.vy
+        if pygame.sprite.spritecollideany(self, vertical_borders):
+            self.vx = -self.vx
+#
+horizontal_borders = pygame.sprite.Group()
+vertical_borders = pygame.sprite.Group()
 
 
-if __name__ == '__main__':
-    main()
+class Border(pygame.sprite.Sprite):
+    # строго вертикальный или строго горизонтальный отрезок
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:  # вертикальная стенка
+            self.add(vertical_borders)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:  # горизонтальная стенка
+            self.add(horizontal_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+# группа, содержащая все спрайты
+all_sprites = pygame.sprite.Group()
+
+Border(5, 5, width - 5, 5)
+Border(5, height - 5, width - 5, height - 5)
+Border(5, 5, 5, height - 5)
+Border(width - 5, 5, width - 5, height - 5)
+
+for i in range(10):
+    Ball(all_sprites, 20, 100, 100)
+
+cursor_image = load_image("arrow.png")
+cursor = pygame.sprite.Sprite(all_sprites)
+cursor.image = cursor_image
+cursor.rect = cursor.image.get_rect()
+
+pygame.mouse.set_visible(False)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEMOTION:
+            # изменяем положение спрайта-стрелки
+            cursor.rect.topleft = event.pos
+    # рисуем курсор только если он в пределах окна
+    if pygame.mouse.get_focused():
+        all_sprites.draw(screen)
+    screen.fill(pygame.Color("white"))
+    all_sprites.update()
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    clock.tick(100)
+
+
+pygame.quit()
